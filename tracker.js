6c49c28d7ecc54f1135fa61886ff18db34ba652d
Daniel Winters <nosuchlogic@gmail.com>
2022-04-26 23:41:27 +1000

    Add Leaflet.js
    Add Leaflet.js

--- tracker.js
+++ tracker.js
@@ -1,3 +1,8 @@
+const map = L.map('map').setView([0, 0], 1);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution})
+tiles.addTo(map);
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 async function getISS() {
