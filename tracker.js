d2c866974cfe4a68a6e21b13dddff9be024b6c15
Daniel Winters <nosuchlogic@gmail.com>
2022-04-27 00:17:24 +1000

    Fix Marker
    Fixed issue with marker not clearing, properly implemented marker updates.

--- tracker.js
+++ tracker.js
@@ -4,19 +4,23 @@ const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Op
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, {attribution})
 tiles.addTo(map);
+
 // Change location marker to picture of ISS
 const icon = L.icon({
   iconUrl: './iss.png',
   iconSize: [50, 32],
   iconAnchor: [25, 16]
 });
+const marker = L.marker([0, 0], { icon: icon}).addTo(map);
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 async function getISS() {
+  // Fetch data from WTIA API
   const response = await fetch(api_url);
   const data = await response.json();
   const {latitude, longitude, altitude, velocity, visibility} = data;
-  L.marker([latitude, longitude], {icon: icon}).addTo(map);
+  marker.setLatLng([latitude, longitude]);
+  // Update DOM elements with data and update map
   document.getElementById('latitude').textContent = latitude;
   document.getElementById('longitude').textContent = longitude;
   document.getElementById('altitude').textContent = altitude;
@@ -30,5 +34,9 @@ async function getISS() {
   }
   
 }
+// Add day/night layer to map
 L.terminator().addTo(map)
+
+// Fetch data every 1 second
 getISS();
+setInterval(getISS, 1000);
