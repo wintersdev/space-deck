b6426f00f6f40522f9cbf1e7a93eb6957b461fb4
Daniel Winters <nosuchlogic@gmail.com>
2022-04-27 23:17:16 +1000

    Implement RSS Feed
    Implement RSS feed to ISS dashboard page.
    Parse RSS feed from NASA into table format.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -63,6 +63,26 @@ async function getISS() {
   }
   
 }
-
 getISS();
 setInterval(getISS, 1000);
+
+const feed = document.getElementById('rss-feeds');
+
+$(document).ready(function()
+{
+  $.ajax({
+    type: "GET",
+    url: "https://www.nasa.gov/rss/dyn/shuttle_station.rss",
+    dataType: "xml",
+    success: parseXml
+  });
+});
+
+function parseXml(xml)
+{
+	$("#rss-feeds").html("");
+	$(xml).find("item").each(function()
+	{
+		$("#rss-feeds").append("<tr><td><a href='"+$(this).find("link").text()+"' target='_blank'><span class='d-flex align-items-center'><i class='bi bi-circle-fill fs-6px text-theme me-2'></i>"+$(this).find("title").text()+"</span></a></td><td><small>"+$(this).find("date").text()+"</small></td><td><span class='badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px' style='min-height: 18px'>NASA</span></td><td><a href='"+$(this).find("link").text()+"' target='_blank' class='text-decoration-none text-white'><i class='bi bi-arrow-right'></i></a></td></tr>");
+	});  
+  }
